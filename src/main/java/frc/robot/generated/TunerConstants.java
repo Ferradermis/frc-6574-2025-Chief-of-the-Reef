80a53880c2456b2389ef60df27fdaf8fd949b4ef
ferradermis-user <ferradermis@gmail.com>
2025-02-23 14:37:51 -0600

    Drive characterization pt 2 electric boogaloo

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -32,7 +32,8 @@ public class TunerConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(2).withKI(0).withKD(0).withKS(0.25).withKV(0.124);
+      new Slot0Configs().withKP(2).withKI(0).withKD(0).withKS(0.17340).withKV(0.68441);
+      
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
@@ -86,7 +87,7 @@ public class TunerConstants {
 
   private static final double kDriveGearRatio = 5.357142857142857;
   private static final double kSteerGearRatio = 21.428571428571427;
-  private static final Distance kWheelRadius = Inches.of(1.62);
+  private static final Distance kWheelRadius = Inches.of(1.618);
 
   private static final boolean kInvertLeftSide = false;
   private static final boolean kInvertRightSide = true;
